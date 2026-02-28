d05b21cb121666ddf8de6698f978eb2b6ac9fb4c
DevKaren23 <karenhelog@gmail.com>
2026-02-27 21:17:16 -0300

    rev

--- backend/database.py
+++ backend/database.py
@@ -2,13 +2,14 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 
+# Carrega variáveis do .env (se existir)
 load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
-        host=os.environ["DB_HOST"],
-        database=os.environ["DB_NAME"],
-        user=os.environ["DB_USER"],
-        password=os.environ["DB_PASSWORD"],
-        port=os.environ["DB_PORT"]
+        host=os.environ.get("DB_HOST"),
+        database=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        port=os.environ.get("DB_PORT")
     )
