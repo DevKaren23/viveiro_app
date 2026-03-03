bda22e62d3f5d1c0d8ec5e9ceebedc42456321c1
DevKaren23 <karenhelog@gmail.com>
2026-03-02 23:42:21 -0300

    add secrets

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,12 @@
 import psycopg2
 import streamlit as st
 
-
 def get_connection():
     return psycopg2.connect(
         host=st.secrets["DB_HOST"],
         database=st.secrets["DB_NAME"],
         user=st.secrets["DB_USER"],
         password=st.secrets["DB_PASSWORD"],
-        port=st.secrets.get("DB_PORT", 5432),
+        port=st.secrets["DB_PORT"],
         sslmode="require"
     )
